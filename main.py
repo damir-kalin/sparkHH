9fcac6e4a46500d4e0c59f70f5954630eabf0850
Sibgatov Damir <77218926+damir-kalin@users.noreply.github.com>
2023-12-07 00:06:06 +0300

    Added parsing depending on the day of the week

--- main.py
+++ main.py
@@ -84,60 +84,62 @@ def transform_to_metrics(vacancies, profession_name, logger):
                         f.replace(f.lower(f.col('profession')), 
                                 f.lit('-'), 
                                 f.lit(" ")))
-            .filter((f.contains(f.col("lower_name"), 
+            .filter(((f.contains(f.col("lower_name"), 
                                 f.lit(profession_name[0]))) | 
                     (f.contains(f.col("lower_name"), 
-                                f.lit(profession_name[1])))) 
+                                f.lit(profession_name[1])))) &
+                    f.col("dt").between(date.today() - timedelta(days=7), date.today() - timedelta(days=1)))
             .withColumn("profession_id", f.lit(profession_name[2]))
             .withColumn("salary", ((f.col("salary_from")+ f.col("salary_to")) / 2))
             .select(
                 f.col("vacancy_id"),
                 f.col("city_id"),
                 f.col("profession_id"),
-                f.col("dt"),
                 f.col("experience"),
                 f.col("schedule"),
                 f.col("skills"),
                 f.col("salary")))
         logger.info("Transform data vacancies")
+
         experience = (vacancies.groupBy(f.col("city_id"),
-                                        f.col("profession_id"),
-                                        f.col("dt"))
+                                        f.col("profession_id")
+                                        )
                                 .pivot("experience",["noExperience", "between1And3", "between3And6", "moreThan6"])
                                 .count()
                                 .select(
                                     f.col("city_id"),
                                     f.col("profession_id"),
-                                    f.col("dt"),
                                     f.col("noExperience").alias("no_experience_cnt"),
                                     f.col("between1And3").alias("between_1_and_3_cnt"),
                                     f.col("between3And6").alias("between_3_and_6_cnt"),
                                     f.col("moreThan6").alias("more_than_6_cnt") 
                                 ))
         logger.info("Trasform data vacancies to experience")
+
         salary = (vacancies.filter(f.col("salary_currency")=="RUR")
                     .groupBy(f.col("city_id"),
-                                    f.col("profession_id"),
-                                    f.col("dt"))
+                                    f.col("profession_id")
+                                    )
                     .pivot("experience",
                             ["noExperience", "between1And3", "between3And6", "moreThan6"])
                     .avg("salary")
                     .select(
                         f.col("city_id"),
                         f.col("profession_id"),
-                        f.col("dt"),
                         f.round(f.col("noExperience"), 2).alias("no_experience_avg_salary"),
                         f.round(f.col("between1And3"), 2).alias("between_1_and_3_avg_salary"),
                         f.round(f.col("between3And6"), 2).alias("between_3_and_6_avg_salary"),
                         f.round(f.col("moreThan6"), 2).alias("more_than_6_avg_salary")
                         ))
         logger.info("Transform data vacancies to salary")
-        schedule = (vacancies.groupBy(f.col("city_id"), f.col("profession_id"), f.col("dt"))
+
+        schedule = (vacancies.groupBy(f.col("city_id")
+                                        ,f.col("profession_id")
+                                        )
                     .pivot("schedule",["fullDay", "shift", "flexible", "remote", "flyInFlyOut"]) 
                     .count()
                     .select(f.col("city_id"),
                             f.col("profession_id"),
-                            f.col("dt"), 
                             f.col("fullDay").alias("full_day_schedule_cnt"),
                             f.col("shift").alias("shift_schedule_cnt"),
                             f.col("remote").alias("remote_schedule_cnt"),
@@ -145,23 +147,25 @@ def transform_to_metrics(vacancies, profession_name, logger):
                             f.col("flyInFlyOut").alias("fly_in_fly_out_schedule_cnt")
                             ))
         logger.info("Transform data vacancies to shedule")
+
         metrics = (vacancies.groupBy(
             f.col("city_id"),
-            f.col("profession_id"),
-            f.col("dt"))
+            f.col("profession_id")
+            )
             .agg(f.count(f.col("vacancy_id")).alias("cnt"),
                     f.round(f.avg(f.col("salary")), 2).alias("avg_salary"))
             .select(
                 f.col("city_id"),
                 f.col("profession_id"),
-                f.col("dt"),
+                f.lit(date.today()).alias("dt"),
                 f.col("cnt"),
                 f.col("avg_salary")))
-        logger.info("Transform data vacancies to metrics")        
+        logger.info("Transform data vacancies to metrics")
+
         metrics = (
-            metrics.join(experience, ["city_id", "profession_id", "dt"])
-                .join(salary, ["city_id", "profession_id", "dt"])
-                .join(schedule, ["city_id", "profession_id", "dt"])
+            metrics.join(experience, ["city_id", "profession_id"])
+                .join(salary, ["city_id", "profession_id"])
+                .join(schedule, ["city_id", "profession_id"])
                 .select(
                     metrics["city_id"],
                     metrics["profession_id"],
@@ -193,34 +197,35 @@ def transform_to_skills_metrics(vacancies, profession_name, logger):
                         f.replace(f.lower(f.col('profession')), 
                                 f.lit('-'), 
                                 f.lit(" ")))
-            .filter((f.contains(f.col("lower_name"), 
+            .filter(((f.contains(f.col("lower_name"), 
                                 f.lit(profession_name[0]))) | 
                     (f.contains(f.col("lower_name"), 
-                                f.lit(profession_name[1])))) 
+                                f.lit(profession_name[1])))) &
+                    f.col("dt").between(date.today() - timedelta(days=7), date.today() - timedelta(days=1)))
             .withColumn("profession_id", f.lit(profession_name[2]))
             .select(
                 f.col("vacancy_id"),
                 f.col("city_id"),
                 f.col("profession_id"),
-                f.col("dt"),
                 f.col("skills")))
         logger.info("Transform data vacancies to skills")
+
         skills = (vacancies.where(f.size(f.col("skills"))>0)
                     .select(f.col("city_id"), 
                             f.col("profession_id"),
-                            f.col("dt"), 
                             f.explode(f.col("skills")).alias("name"))
                     .groupBy(f.col("city_id"), 
                                 f.col("profession_id"),
-                                f.col('dt'),
                                 f.col("name"))
                     .count()
                     .select(f.col("city_id"), 
                             f.col("profession_id"),
-                            f.col("dt"), 
+                            f.lit(date.today()).alias("dt"), 
                             f.col("name"),
                             f.col("count").alias("cnt")
                             )
+                    .orderBy(f.col('cnt').desc())
+                    .limit(50)
                     )
         logger.info("Return skills")
         return skills
@@ -243,11 +248,15 @@ if __name__ == "__main__":
     if profession:
         for profession_name in profession[["name", "en_name", "profession_id"]].collect():
             logger.info(f"Run with data profession - {profession_name}")
-            run_parse(profession_name[0], logger)
-            vacancies = get_table(spark, 'vacancies', config, logger)
-            metrics = transform_to_metrics(vacancies, profession_name, logger)
-            load_data(metrics, 'metrics', config, logger)
-            skills = transform_to_skills_metrics(vacancies, profession_name, logger)
-            load_data(skills, 'skills', config, logger)
-            logger.info(f"Job with {profession_name} successfully")    
+            if date.today().isoweekday()== 1:
+                run_parse(profession_name[0], config, logger)
+                vacancies = get_table(spark, 'vacancies', config, logger)
+                metrics = transform_to_metrics(vacancies, profession_name, logger)
+                load_data(metrics, 'metrics', config, logger)
+                skills = transform_to_skills_metrics(vacancies, profession_name, logger)
+                load_data(skills, 'skills', config, logger)
+                logger.info(f"Job with {profession_name} successfully")
+            else:
+                run_parse(profession_name[0], config, logger)
+                logger.info(f"Job with {profession_name} successfully")
         logger.info("Spark job successfully")
