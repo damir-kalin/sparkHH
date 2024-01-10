36f1f3dc4b11e03cbe052e830cd0cf8a7b7a7c7f
Sibgatov Damir <77218926+damir-kalin@users.noreply.github.com>
2024-01-10 21:41:00 +0300

    Refactor 10.11.24 3

--- main.py
+++ main.py
@@ -79,115 +79,137 @@ def run_parse(profession_name, config, logger):
         logger.error("Error parsing")
 
 def transform_to_metrics(vacancies, profession_name, logger):
-    # try:
     vacancies = (vacancies.withColumn("lower_name", 
-                    f.replace(f.lower(f.col('profession')), 
-                            f.lit('-'), 
-                            f.lit(" ")))
-        .filter(((f.contains(f.col("lower_name"), 
-                            f.lit(profession_name[0]))) | 
-                (f.contains(f.col("lower_name"), 
-                            f.lit(profession_name[1])))) &
-                f.col("dt").between(date.today() - timedelta(days=7), date.today() - timedelta(days=1)))
-        .withColumn("profession_id", f.lit(profession_name[2]))
-        .withColumn("salary", ((f.col("salary_from")+ f.col("salary_to")) / 2))
-        .select(
-            f.col("vacancy_id"),
-            f.col("city_id"),
-            f.col("profession_id"),
-            f.col("experience"),
-            f.col("schedule"),
-            f.col("skills"),
-            f.col("salary")))
-    logger.info("Transform data vacancies")
-
-    experience = (vacancies.groupBy(f.col("city_id"),
-                                    f.col("profession_id")
-                                    )
-                            .pivot("experience",["noExperience", "between1And3", "between3And6", "moreThan6"])
-                            .count()
-                            .select(
-                                f.col("city_id"),
-                                f.col("profession_id"),
-                                f.col("noExperience").alias("no_experience_cnt"),
-                                f.col("between1And3").alias("between_1_and_3_cnt"),
-                                f.col("between3And6").alias("between_3_and_6_cnt"),
-                                f.col("moreThan6").alias("more_than_6_cnt") 
-                            ))
-    logger.info("Trasform data vacancies to experience")
-
-    salary = (vacancies.filter(f.col("salary_currency")=="RUR")
-                .groupBy(f.col("city_id"),
-                                f.col("profession_id")
-                                )
-                .pivot("experience",
-                        ["noExperience", "between1And3", "between3And6", "moreThan6"])
-                .avg("salary")
-                .select(
-                    f.col("city_id"),
-                    f.col("profession_id"),
-                    f.round(f.col("noExperience"), 2).alias("no_experience_avg_salary"),
-                    f.round(f.col("between1And3"), 2).alias("between_1_and_3_avg_salary"),
-                    f.round(f.col("between3And6"), 2).alias("between_3_and_6_avg_salary"),
-                    f.round(f.col("moreThan6"), 2).alias("more_than_6_avg_salary")
-                    ))
-    logger.info("Transform data vacancies to salary")
-
-    schedule = (vacancies.groupBy(f.col("city_id")
-                                    ,f.col("profession_id")
-                                    )
-                .pivot("schedule",["fullDay", "shift", "flexible", "remote", "flyInFlyOut"]) 
-                .count()
-                .select(f.col("city_id"),
-                        f.col("profession_id"),
-                        f.col("fullDay").alias("full_day_schedule_cnt"),
-                        f.col("shift").alias("shift_schedule_cnt"),
-                        f.col("remote").alias("remote_schedule_cnt"),
-                        f.col("flexible").alias("flexible_schedule_cnt"),
-                        f.col("flyInFlyOut").alias("fly_in_fly_out_schedule_cnt")
-                        ))
-    logger.info("Transform data vacancies to shedule")
-
-    metrics = (vacancies.groupBy(
-        f.col("city_id"),
-        f.col("profession_id")
-        )
-        .agg(f.count(f.col("vacancy_id")).alias("cnt"),
-                f.round(f.avg(f.col("salary")), 2).alias("avg_salary"))
-        .select(
-            f.col("city_id"),
-            f.col("profession_id"),
-            f.lit(date.today()).alias("dt"),
-            f.col("cnt"),
-            f.col("avg_salary")))
-    logger.info("Transform data vacancies to metrics")
-
-    metrics = (
-        metrics.join(experience, ["city_id", "profession_id"])
-            .join(salary, ["city_id", "profession_id"])
-            .join(schedule, ["city_id", "profession_id"])
+                        f.replace(f.lower(f.col('profession')), 
+                                f.lit('-'), 
+                                f.lit(" ")))
+            .filter(((f.contains(f.col("lower_name"), 
+                                f.lit(profession_name[0]))) | 
+                    (f.contains(f.col("lower_name"), 
+                                f.lit(profession_name[1])))) &
+                    f.col("dt").between(date.today() - timedelta(days=7), date.today() - timedelta(days=1)))
+            .withColumn("profession_id", f.lit(profession_name[2]))
+            .withColumn("salary", ((f.col("salary_from")+ f.col("salary_to")) / 2))
             .select(
-                metrics["city_id"],
-                metrics["profession_id"],
-                metrics["dt"],
-                metrics["cnt"],
-                experience["no_experience_cnt"],
-                experience["between_1_and_3_cnt"],
-                experience["between_3_and_6_cnt"],
-                experience["more_than_6_cnt"],
-                metrics["avg_salary"],
-                salary["no_experience_avg_salary"],
-                salary["between_1_and_3_avg_salary"],
-                salary["between_3_and_6_avg_salary"],
-                salary["more_than_6_avg_salary"],
-                schedule["flexible_schedule_cnt"],
-                schedule["remote_schedule_cnt"],
-                schedule["full_day_schedule_cnt"],
-                schedule["shift_schedule_cnt"],
-                schedule["fly_in_fly_out_schedule_cnt"]
-            ))
-    logger.info("Return metrics")
-    return metrics
+                f.col("vacancy_id"),
+                f.col("city_id"),
+                f.col("profession_id"),
+                f.col("experience"),
+                f.col("schedule"),
+                f.col("skills"),
+                f.col("salary")))
+    logger.info("Transform data vacancies")
+    vacancies.show()
+
+    # try:
+        # vacancies = (vacancies.withColumn("lower_name", 
+        #                 f.replace(f.lower(f.col('profession')), 
+        #                         f.lit('-'), 
+        #                         f.lit(" ")))
+        #     .filter(((f.contains(f.col("lower_name"), 
+        #                         f.lit(profession_name[0]))) | 
+        #             (f.contains(f.col("lower_name"), 
+        #                         f.lit(profession_name[1])))) &
+        #             f.col("dt").between(date.today() - timedelta(days=7), date.today() - timedelta(days=1)))
+        #     .withColumn("profession_id", f.lit(profession_name[2]))
+        #     .withColumn("salary", ((f.col("salary_from")+ f.col("salary_to")) / 2))
+        #     .select(
+        #         f.col("vacancy_id"),
+        #         f.col("city_id"),
+        #         f.col("profession_id"),
+        #         f.col("experience"),
+        #         f.col("schedule"),
+        #         f.col("skills"),
+        #         f.col("salary")))
+        # logger.info("Transform data vacancies")
+
+        # experience = (vacancies.groupBy(f.col("city_id"),
+        #                                 f.col("profession_id")
+        #                                 )
+        #                         .pivot("experience",["noExperience", "between1And3", "between3And6", "moreThan6"])
+        #                         .count()
+        #                         .select(
+        #                             f.col("city_id"),
+        #                             f.col("profession_id"),
+        #                             f.col("noExperience").alias("no_experience_cnt"),
+        #                             f.col("between1And3").alias("between_1_and_3_cnt"),
+        #                             f.col("between3And6").alias("between_3_and_6_cnt"),
+        #                             f.col("moreThan6").alias("more_than_6_cnt") 
+        #                         ))
+        # logger.info("Trasform data vacancies to experience")
+
+        # salary = (vacancies.filter(f.col("salary_currency")=="RUR")
+        #             .groupBy(f.col("city_id"),
+        #                             f.col("profession_id")
+        #                             )
+        #             .pivot("experience",
+        #                     ["noExperience", "between1And3", "between3And6", "moreThan6"])
+        #             .avg("salary")
+        #             .select(
+        #                 f.col("city_id"),
+        #                 f.col("profession_id"),
+        #                 f.round(f.col("noExperience"), 2).alias("no_experience_avg_salary"),
+        #                 f.round(f.col("between1And3"), 2).alias("between_1_and_3_avg_salary"),
+        #                 f.round(f.col("between3And6"), 2).alias("between_3_and_6_avg_salary"),
+        #                 f.round(f.col("moreThan6"), 2).alias("more_than_6_avg_salary")
+        #                 ))
+        # logger.info("Transform data vacancies to salary")
+
+        # schedule = (vacancies.groupBy(f.col("city_id")
+        #                                 ,f.col("profession_id")
+        #                                 )
+        #             .pivot("schedule",["fullDay", "shift", "flexible", "remote", "flyInFlyOut"]) 
+        #             .count()
+        #             .select(f.col("city_id"),
+        #                     f.col("profession_id"),
+        #                     f.col("fullDay").alias("full_day_schedule_cnt"),
+        #                     f.col("shift").alias("shift_schedule_cnt"),
+        #                     f.col("remote").alias("remote_schedule_cnt"),
+        #                     f.col("flexible").alias("flexible_schedule_cnt"),
+        #                     f.col("flyInFlyOut").alias("fly_in_fly_out_schedule_cnt")
+        #                     ))
+        # logger.info("Transform data vacancies to shedule")
+
+        # metrics = (vacancies.groupBy(
+        #     f.col("city_id"),
+        #     f.col("profession_id")
+        #     )
+        #     .agg(f.count(f.col("vacancy_id")).alias("cnt"),
+        #             f.round(f.avg(f.col("salary")), 2).alias("avg_salary"))
+        #     .select(
+        #         f.col("city_id"),
+        #         f.col("profession_id"),
+        #         f.lit(date.today()).alias("dt"),
+        #         f.col("cnt"),
+        #         f.col("avg_salary")))
+        # logger.info("Transform data vacancies to metrics")
+
+        # metrics = (
+        #     metrics.join(experience, ["city_id", "profession_id"])
+        #         .join(salary, ["city_id", "profession_id"])
+        #         .join(schedule, ["city_id", "profession_id"])
+        #         .select(
+        #             metrics["city_id"],
+        #             metrics["profession_id"],
+        #             metrics["dt"],
+        #             metrics["cnt"],
+        #             experience["no_experience_cnt"],
+        #             experience["between_1_and_3_cnt"],
+        #             experience["between_3_and_6_cnt"],
+        #             experience["more_than_6_cnt"],
+        #             metrics["avg_salary"],
+        #             salary["no_experience_avg_salary"],
+        #             salary["between_1_and_3_avg_salary"],
+        #             salary["between_3_and_6_avg_salary"],
+        #             salary["more_than_6_avg_salary"],
+        #             schedule["flexible_schedule_cnt"],
+        #             schedule["remote_schedule_cnt"],
+        #             schedule["full_day_schedule_cnt"],
+        #             schedule["shift_schedule_cnt"],
+        #             schedule["fly_in_fly_out_schedule_cnt"]
+        #         ))
+        # logger.info("Return metrics")
+        # return metrics
     # except:
     #     logger.error("Error return metrics")
 
@@ -249,13 +271,13 @@ if __name__ == "__main__":
         for profession_name in profession[["name", "en_name", "profession_id"]].collect():
             logger.info(f"Run with data profession - {profession_name}")
             if date.today().isoweekday()== 3:
-                run_parse(profession_name[0], config, logger)
+                # run_parse(profession_name[0], config, logger)
                 vacancies = get_table(spark, 'vacancies', config, logger)
                 metrics = transform_to_metrics(vacancies, profession_name, logger)
-                load_data(metrics, 'metrics', config, logger)
-                skills = transform_to_skills_metrics(vacancies, profession_name, logger)
-                load_data(skills, 'skills', config, logger)
-                logger.info(f"Job with {profession_name} successfully")
+                # load_data(metrics, 'metrics', config, logger)
+                # skills = transform_to_skills_metrics(vacancies, profession_name, logger)
+                # load_data(skills, 'skills', config, logger)
+                # logger.info(f"Job with {profession_name} successfully")
             else:
                 run_parse(profession_name[0], config, logger)
                 logger.info(f"Job with {profession_name} successfully")
